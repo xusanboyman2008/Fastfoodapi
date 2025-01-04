15b64c8b06d984f46b493a614d23e5716562c777
xusanboyman2008 <abdulkhaevtolibjon55@gmail.com>
2025-01-04 23:55:14 +0500

    hello

--- RESTFRAMEWORKTEST/settings.py
+++ RESTFRAMEWORKTEST/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import datetime
 from pathlib import Path
-
+import dj_database_url
 import AUTH_USER
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)wx((d2@ja+3gieh#7ypiyjli7z_748=bdhzs38dzkc8_7g@cx'
-
+# SECRET_KEY = 'django-insecure-)wx((d2@ja+3gieh#7ypiyjli7z_748=bdhzs38dzkc8_7g@cx'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False) == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(' ')
 
 # Application definition
 
@@ -74,7 +75,8 @@ ROOT_URLCONF = 'RESTFRAMEWORKTEST.urls'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': BASE_DIR / 'db.sqlite3', }}
-
+database_url = os.environ.get('DATABASE_URL')
+DATABASES['default'] = dj_database_url.parse(database_url)
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
