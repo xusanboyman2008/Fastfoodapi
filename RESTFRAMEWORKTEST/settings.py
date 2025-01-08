3eed523b066577328fd693c3414e8db466f1308f
xusanboyman2008 <abdulkhaevtolibjon55@gmail.com>
2025-01-08 22:08:53 +0500

    hello

--- RESTFRAMEWORKTEST/settings.py
+++ RESTFRAMEWORKTEST/settings.py
@@ -53,7 +53,6 @@ MIDDLEWARE = [
     # Temporarily comment out
 ]
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = ['*']
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
